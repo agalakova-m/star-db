8ddb5ffbacedc4987fd9190c8d2bd9771ac6e9e1
Marina Agalakova <agalakova.m.a@gmail.com>
2020-03-01 23:09:33 +0300

    add preloader

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './spinner.css';
+
+const Spinner = () => {
+  return (
+    <div className="spinner loadingio-spinner-rolling-vms8ladxu1k">
+      <div className="ldio-poyizufrre">
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
