a0be613bb57413567ff7b53da80ed17eaa9375b7
Marina Agalakova <agalakova.m.a@gmail.com>
2020-03-31 21:46:37 +0300

    refactoring: getting data

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,9 +6,15 @@ import PeoplePage from '../people-page';
 import ErrorButton from '../error-button';
 import ErrorIndicator from '../error-indicator/error-indicator';
 
+import ItemList from '../item-list/item-list';
+import PersonDetails from '../person-details/person-details';
+
+import SwapiService from '../../services/swapi-service';
+
 import './app.css';
 
 export default class App extends Component {
+  swapiService = new SwapiService();
   state = {
     hasError: false,
   };
@@ -29,8 +35,30 @@ export default class App extends Component {
           <ErrorButton />
         </div>
         <PeoplePage />
-        <PeoplePage />
-        <PeoplePage />
+
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList
+              onItemSelected={this.onPersonSelected}
+              getData={this.swapiService.getAllPlanets}
+            />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson} />
+          </div>
+        </div>
+
+        <div className="row mb2">
+          <div className="col-md-6">
+            <ItemList
+              onItemSelected={this.onPersonSelected}
+              getData={this.swapiService.getAllStarships}
+            />
+          </div>
+          <div className="col-md-6">
+            <PersonDetails personId={this.state.selectedPerson} />
+          </div>
+        </div>
       </div>
     );
   }
